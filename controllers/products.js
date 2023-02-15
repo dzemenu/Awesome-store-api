5b81c6136d1b1f6ff1b825489a3a41b2dc54cb9a
dilu kefale <dilu.kefale@gmail.com>
2023-02-15 15:15:56 +0300

    data auto populate implmented

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,19 @@
-
-
-const getAllProductsStatic=async(req,res)=>{
-    
-    await res.status(200).json({msg:'products static route test'})
-}
-const getAllProducts=async(req,res)=>{
-    await res.status(200).json({msg:'products route'})
-}
-module.exports={
-    getAllProducts,getAllProductsStatic
-}
+const Product = require("../models/product");
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find();
+  await res.status(200).json({ products });
+};
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  await res.status(200).json({ products, nbHits: products.length });
+};
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
